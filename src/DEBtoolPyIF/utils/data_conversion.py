169906bb11ad6853a070f6b479c8866560034f83
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-08-28 15:11:14 +0100

    Functions to convert strings and numeric arrays to MATLAB

--- src/DEBtoolPyIF/utils/data_conversion.py
+++ src/DEBtoolPyIF/utils/data_conversion.py
@@ -1,3 +1,25 @@
+import numpy as np
+
+
+def convert_string_to_matlab(string: str) -> str:
+    return string.__repr__()
+
+
+def convert_numeric_array_to_matlab(array: [int, float, np.array]):
+    if np.size(array) == 1:
+        if isinstance(array, np.ndarray):
+            return array.item()
+        else:
+            return array
+    else:
+        rows = []
+        for row in array:  # flatten higher dims into 2D
+            row_str = " ".join(str(x) for x in row)
+            rows.append(row_str)
+        matlab_code = "; ".join(rows)
+        return f"[{matlab_code}]"
+
+
 def convert_list_of_strings_to_matlab(list_data: list, double_brackets: bool = False):
     matlab_code = '{' + list_data.__repr__()[1:-1] + '}'
     if double_brackets:
--- src/DEBtoolPyIF/utils/mydata_code_generation.py
+++ src/DEBtoolPyIF/utils/mydata_code_generation.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+from .data_conversion import convert_string_to_matlab
 
 def check_files_exist_in_folder(folder_name, files):
     if not isinstance(files, (list, tuple)):
@@ -11,31 +11,43 @@ def check_files_exist_in_folder(folder_name, files):
     return True, "All good!"
 
 
-def generate_dataset_code(var_name, formatted_data):
-    return
+def generate_data_code(var_name: str, converted_data: str, units: str, label: str, title=None, comment=None,
+                       bibkey=None):
+    data_code = f"data.{var_name} = {converted_data};\n" \
+                   f"units.{var_name} = {units}; " \
+                   + f"label.{var_name} = {label}; "
+    if comment:
+        data_code += f"comment.{var_name} = '{comment}'; "
+    if title:
+        data_code += f"title.{var_name} = '{title}'; "
+    if bibkey:
+        data_code += f"bibkey.{var_name} = '{bibkey}';"
+
+    return data_code
 
 
-def generate_aux_data_code(var_name, formatted_data, struct_name, label, units):
+def generate_aux_data_code(var_name, converted_data, struct_name, label, units):
     """
     Generates the code for an auxiliary dataset
     :param var_name: the name of the dataset
-    :param formatted_data: the data already formatted in MATLAB syntax
+    :param converted_data: the data already formatted in MATLAB syntax
     :param struct_name: the name of the struct where to save the auxiliary data
     :param label: the label of the auxiliary data
     :param units: the units of the auxiliary data
     :return: the MATLAB code for the auxiliary data
     """
-    formatted_aux_data = f"\n{struct_name}.{var_name} = {formatted_data}; " \
-                        f"units.{struct_name}.{var_name} = '{units}'; " \
-                        f"label.{struct_name}.{var_name} = '{label}'; \n"
-    return formatted_aux_data
+    aux_data_code = f"\n{struct_name}.{var_name} = {converted_data}; " \
+                    f"units.{struct_name}.{var_name} = {units}; " \
+                    f"label.{struct_name}.{var_name} = {label}; \n"
+    return aux_data_code
 
 
 def generate_meta_data_code(var_name, formatted_data):
     return f"metaData.{var_name} = {formatted_data}; \n"
 
 
-def generate_tier_variable_code(var_name, formatted_data, label, units='-', bibkey='', comment='', pars_init_access=False):
+def generate_tier_variable_code(var_name, formatted_data, label, units='-', bibkey='', comment='',
+                                pars_init_access=False):
     s = f"data.{var_name} = 10; " \
         f"units.{var_name} = '-'; " \
         f"label.{var_name} = 'Tier structure variable'; "
@@ -43,7 +55,8 @@ def generate_tier_variable_code(var_name, formatted_data, label, units='-', bibk
         s += f"comment.{var_name} = '{comment}'; "
     if bibkey:
         s += f"bibkey.{var_name} = '{bibkey}';"
-    s += generate_aux_data_code(var_name, formatted_data, struct_name='tiers', label=label, units=units)
+    s += generate_aux_data_code(var_name, formatted_data, struct_name='tiers',
+                                label=convert_string_to_matlab(label), units=convert_string_to_matlab(units))
     if pars_init_access:
         s += generate_meta_data_code(var_name, formatted_data=f"tiers.{var_name}")
     return s

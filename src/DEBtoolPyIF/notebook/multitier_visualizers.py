979c043ecbb5b1cf1c8ab8af2b070ec6b90a1659
Diogo Oliveira <diogo.oliveira898@gmail.com>
2025-09-22 10:49:42 +0100

    TierVisualizer now works with update tiered data structured

--- src/DEBtoolPyIF/notebook/multitier_visualizers.py
+++ src/DEBtoolPyIF/notebook/multitier_visualizers.py
@@ -1,114 +1,175 @@
 import matplotlib.pyplot as plt
 import ipywidgets as widgets
+import numpy as np
 from IPython.display import display, clear_output
 
 from ..multitier.procedure import MultiTierStructure
+from ..data_sources.collection import DataCollection
 
 
 class TierVisualizer:
+    MAX_SUBPLOT_COLS = 5
+    SUBPLOT_SIZE = 4
+
     def __init__(self, tier_structure: MultiTierStructure, plotting_functions: dict):
         self.tier_structure = tier_structure
-        # TODO: Maybe do checkboxes for selecting tiers instead of dropdown widgets
-        self.tier_sample_selectors = {}
+        self.tier_entity_selectors = {}
+        self.tier_group_selectors = {}
+        data_types = []
         for tier_name, tier in self.tier_structure.tiers.items():
-            self.tier_sample_selectors[tier_name] = widgets.Dropdown(
-                options=['all'] + tier.tier_sample_list,
-                description=f'{tier_name}:'
+            self.tier_entity_selectors[tier_name] = widgets.SelectMultiple(
+                options=tier.tier_entities,
+                value=tier.tier_entities,
+                description=f'{tier_name} entity:'
             )
-        self.ind_selector = widgets.Dropdown(
-            options=self.tier_structure.data.individuals,
-            description='Individual:'
-        )
-        self.group_selector = widgets.Dropdown(
-            options=self.tier_structure.data.groups,
-            description='Group:'
-        )
+            self.tier_group_selectors[tier_name] = widgets.SelectMultiple(
+                options=tier.tier_groups,
+                value=tier.tier_groups,
+                description=f'{tier_name} group:'
+            )
+            data_types.extend([f"{tier_name} {dt}" for dt in tier.data.data_types])
+
         self.data_type_selector = widgets.Dropdown(
-            options=self.tier_structure.data.data_types,
-            description='Data Type:'
+            options=data_types,
+            value=data_types[1],
+            description='Data:'
         )
+
+        # TODO: Add argument for plotting function arguments
         self.plotting_functions = plotting_functions
         self.output_widget = widgets.Output()  # Output widget to capture and display plots
 
+    def create_figure(self, n_subplots):
+        n_cols = self.MAX_SUBPLOT_COLS if n_subplots > 5 else n_subplots
+        n_rows = n_subplots // n_cols + (n_subplots % n_cols > 0)
+        fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols,
+                                 figsize=(self.SUBPLOT_SIZE * n_cols, self.SUBPLOT_SIZE * n_rows),
+                                 tight_layout=True)
+        # Hide unused axes
+        for j in range(n_subplots, n_rows * n_cols):
+            axes.flat[j].axis('off')
+
+        return fig, axes
+
+    def plot_predictions_of_entity_list(self, axes, plotting_function, pars_entity_list,
+                                        data_collection: DataCollection,
+                                        data_type: str, pars_tier: str, data_tier: str):
+        flat_axes = np.atleast_1d(axes).ravel()
+        ax_idx = 0
+        for p_id in pars_entity_list:
+            pars = self.tier_structure.get_full_pars_dict(tier_name=pars_tier, entity_id=p_id, include_tier=True)
+            data_entity_list = self.tier_structure.entities_in_other_tier_from_entity_list(
+                tier_name=pars_tier, other_tier=data_tier, entity_list=[p_id]
+            )
+
+            for d_id in data_entity_list:
+                data_source_name = data_collection.get_data_source_of_entity(entity_id=d_id, data_type=data_type)[0]
+                data_source = data_collection.data_sources[data_source_name]
+                plotting_function(ax=flat_axes[ax_idx],
+                                  data_source=data_source,
+                                  data_tier=data_tier,
+                                  data_entity_id=d_id,
+                                  pars=pars,
+                                  pars_tier=pars_tier,
+                                  pars_entity_id=p_id,
+                                  tier_structure=self.tier_structure,
+                                  )
+                ax_idx += 1
+
+    def plot_predictions_of_group_list(self, axes, plotting_function, group_list, data_collection: DataCollection,
+                                       data_type: str, pars_tier: str, data_tier: str):
+        flat_axes = np.atleast_1d(axes).ravel()
+        ax_idx = 0
+        for g_id in group_list:
+            # Get data source of group id
+            data_source_name = data_collection.get_data_source_of_group(group_id=g_id, data_type=data_type)[0]
+            data_source = data_collection.data_sources[data_source_name]
+            # Get full parameter dict of pars_tier based on group id
+            data_entity_list = data_collection.get_entity_list_of_group(group_id=g_id)
+            data_entity_pars_dict = {}
+            pars_entity_list = []
+            for d_id in data_entity_list:
+                pars_entity_of_data_entity = self.tier_structure.entities_in_other_tier_from_entity_list(
+                    tier_name=data_tier, other_tier=pars_tier, entity_list=[d_id]
+                )[0]
+                pars_dict = self.tier_structure.get_full_pars_dict(tier_name=pars_tier,
+                                                                   entity_id=pars_entity_of_data_entity,
+                                                                   include_tier=True)
+                pars_entity_list.append(pars_entity_of_data_entity)
+                data_entity_pars_dict[d_id] = pars_dict
+            plotting_function(ax=flat_axes[ax_idx],
+                              data_source=data_source,
+                              data_tier=data_tier,
+                              data_group_id=g_id,
+                              data_entity_pars_dict=data_entity_pars_dict,
+                              pars_tier=pars_tier,
+                              pars_entity_list=pars_entity_list,
+                              tier_structure=self.tier_structure,
+                              )
+            ax_idx += 1
+
     def plot_tier_data_and_predictions(self, tier_name):
-        def display_data_and_predictions(tier_sample, data_type):
+        def display_data_and_predictions(selected_entity_list, tier_and_data_type):
             with self.output_widget:  # Use the output widget as the context for plot display
                 clear_output()  # Clear the previous plots
-                if tier_sample != 'all':
-                    tier_sample = [tier_sample]
-                ind_list = self.tier_structure.ind_list_from_tier_sample_list(tier_name, tier_sample)
-                if data_type in self.tier_structure.data.ind_data_types:
-                    # Create subplots, max 5 per row
-                    n_cols = 5 if len(ind_list) > 5 else len(ind_list)
-                    n_rows = len(ind_list) // n_cols + (len(ind_list) % n_cols > 0)
-                    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(4 * n_cols, 4 * n_rows),
-                                             tight_layout=True)
-                    for i, ind_id in enumerate(ind_list):
-                        if len(ind_list) == 1:
-                            ax = axes
-                        elif n_rows == 1:
-                            ax = axes[i % n_cols]
-                        else:
-                            ax = axes[i // n_cols, i % n_cols]
-                        self.plotting_functions[data_type](
-                            ax=ax,
-                            tier_structure=self.tier_structure,
-                            tier_name=tier_name,
-                            ind_id=ind_id
-                        )
+                data_tier, data_type = tier_and_data_type.split()
+                data_collection = self.tier_structure.data[data_tier]
+
+                # ind_list = self.tier_structure.ind_list_from_tier_sample_list(tier_name, tier_entity_list)
+                if data_type in data_collection.entity_data_types:
+
+                    # Create subplots
+                    if self.tier_structure.is_tier_below(tier_name=tier_name, other_tier=data_tier):
+                        n_subplots = len(self.tier_structure.entities_in_other_tier_from_entity_list(
+                            tier_name=tier_name, other_tier=data_tier, entity_list=selected_entity_list
+                        ))
+                    else:
+                        n_subplots = len(selected_entity_list)
+                    fig, axes = self.create_figure(n_subplots=n_subplots)
+
+                    self.plot_predictions_of_entity_list(
+                        axes=axes,
+                        plotting_function=self.plotting_functions[data_tier][data_type],
+                        pars_entity_list=selected_entity_list,
+                        data_collection=data_collection,
+                        data_type=data_type,
+                        pars_tier=tier_name,
+                        data_tier=data_tier
+                    )
+
                     # TODO: axis labels should only be displayed on the outer axes
-                else:
-                    group_ids_list = sorted(self.tier_structure.data.get_group_list_from_ind_list(ind_list))
-                    # Create subplots, max 5 per row
-                    # TODO: Creating the figure should be a method
-                    n_cols = 5 if len(ind_list) > 5 else len(ind_list)
-                    n_rows = len(group_ids_list) // n_cols + (len(group_ids_list) % n_cols > 0)
-                    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols, figsize=(4 * n_cols, 4 * n_rows),
-                                             tight_layout=True)
-                    for i, group_id in enumerate(group_ids_list):
-                        # TODO: Getting the axis should be a method
-                        if len(group_ids_list) == 1:
-                            ax = axes
-                        elif n_rows == 1:
-                            ax = axes[i % n_cols]
-                        else:
-                            ax = axes[i // n_cols, i % n_cols]
-                        self.plotting_functions[data_type](
-                            ax=ax,
-                            tier_structure=self.tier_structure,
-                            tier_name=tier_name,
-                            group_id=group_id
+                elif data_type in data_collection.group_data_types:
+                    if tier_name == data_tier:
+                        data_entity_list = list(selected_entity_list)
+                    elif self.tier_structure.is_tier_below(tier_name=tier_name, other_tier=data_tier):
+                        data_entity_list = self.tier_structure.entities_in_other_tier_from_entity_list(
+                            tier_name=tier_name, other_tier=data_tier, entity_list=selected_entity_list
                         )
-                # Hide unused axes
-                for j in range(i + 1, n_rows * n_cols):
-                    axes.flat[j].axis('off')
+                    else:
+                        print('Behaviour is not defined for plotting predictions of a group data source of a tier '
+                              'above of the parameters tier.')
+                        return
+
+                    group_list = data_collection.get_group_list_from_entity_list(data_entity_list)
+                    fig, axes = self.create_figure(n_subplots=len(group_list))
+
+                    self.plot_predictions_of_group_list(
+                        axes=axes,
+                        plotting_function=self.plotting_functions[data_tier][data_type],
+                        group_list=group_list,
+                        data_collection=data_collection,
+                        data_type=data_type,
+                        pars_tier=tier_name,
+                        data_tier=data_tier
+                    )
+                else:
+                    raise Exception(f'Data type {data_type} is not an Entity or Group Data Source.')
 
                 # fig.show()
                 fig.canvas.draw()
 
         # This time, explicitly display the interactive widget and the output widget
         interactive_widget = widgets.interactive(display_data_and_predictions,
-                                                 tier_sample=self.tier_sample_selectors[tier_name],
-                                                 data_type=self.data_type_selector)
-        display(interactive_widget, self.output_widget)
-
-    def plot_ind_data_predictions(self, tier_name):
-        def display_ind_data_predictions(ind_id, data_type):
-            if data_type not in self.tier_structure.data.data_types:
-                print(f"Data type '{data_type}' is not an individual data type")
-                return
-            with self.output_widget:
-                clear_output(wait=True)
-                if data_type in self.tier_structure.data.ind_data_types:
-                    fig, ax = plt.subplots(figsize=(16, 9), tight_layout=True)
-                    self.plotting_functions[data_type](
-                        ax=ax,
-                        tier_structure=self.tier_structure,
-                        tier_name=tier_name,
-                        ind_id=ind_id
-                    )
-
-        interactive_widget = widgets.interactive(display_ind_data_predictions,
-                                                 ind_id=self.ind_selector,
-                                                 data_type=self.data_type_selector)
+                                                 selected_entity_list=self.tier_entity_selectors[tier_name],
+                                                 tier_and_data_type=self.data_type_selector)
         display(interactive_widget, self.output_widget)

63b56300db62ae21e4ae1d56490260eb25db350d
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-04-09 18:56:48 +0100

    mydata set temperature sections for templates

---
+++ src/DEBtoolPyIF/estimation_files/mydata_temperature_sections.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from DEBtoolPyIF import MyDataSection, BaseMyDataState
+
+
+class SetTypicalTemperatureForAllDatasetsSection(MyDataSection):
+    """
+    This section provides code to set the temperature of all datasets to the typical temperature defined in
+    `metadata.T_typical'.
+    """
+    key = "set_temperature_block"
+
+    def render(self, _context, _state: BaseMyDataState) -> str:
+        return """%% Set temperature metadata
+temp = struct();
+for i = 1:length(metaData.data_fields)
+    field = metaData.data_fields{i};
+
+    if ~isfield(temp, field)
+        temp.(field) = metaData.T_typical;
+        units.temp.(field) = 'K';
+        label.temp.(field) = 'temperature';
+    end
+end"""

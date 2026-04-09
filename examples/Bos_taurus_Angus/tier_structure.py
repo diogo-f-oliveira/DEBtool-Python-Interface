8052aea6d34e3cce0c4df42adf414a9c22f289f5
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-04-09 19:04:12 +0100

    Update Angus example to use new template classes

--- examples/Bos_taurus_Angus/tier_structure.py
+++ examples/Bos_taurus_Angus/tier_structure.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 
 from DEBtoolPyIF.data_sources.collection import DataCollection
-from DEBtoolPyIF.multitier import MultiTierStructure, TierHierarchy
+from DEBtoolPyIF.estimation_files import EstimationTemplates, CopyFileTemplate, RunSubstitutionTemplate
+from DEBtoolPyIF.estimation_files.mydata_temperature_sections import SetTypicalTemperatureForAllDatasetsSection
+from DEBtoolPyIF.estimation_files.mydata_metadata_sections import SaveDataFieldsByVariateTypeSection
+from DEBtoolPyIF.multitier import MultiTierStructure, TierHierarchy, MultitierMyDataSubstitutionTemplate, \
+    MultitierParsInitSubstitutionTemplate, build_estimation_templates_from_folder
 
 
 HERE = Path(__file__).resolve().parent
-ESTIMATION_FOLDER = str(HERE / 'multitier')
-BASE_TEMPLATE_FOLDER = str(HERE / 'templates')
+ESTIMATION_FOLDER = HERE / 'multitier'
+BASE_TEMPLATE_FOLDER = HERE / 'templates'
 
+SPECIES_NAME = 'Bos_taurus_Angus'
 TIER_NAMES = ['breed', 'diet', 'individual']
 
 
@@ -20,6 +25,33 @@ def generate_entity_hierarchy(data: DataCollection) -> TierHierarchy:
         ind_data = twds.get_entity_data(ind_id).iloc[0]
         paths.append({'breed': 'male', 'diet': f"{ind_data['diet']}", 'individual': ind_id})
     return TierHierarchy.from_paths(tier_names=TIER_NAMES, paths=paths)
+
+
+def build_estimation_templates(species_name: str) -> dict[str, EstimationTemplates]:
+    """Create the template files for the example."""
+    mydata_sections = MultitierMyDataSubstitutionTemplate.default_sections() + (
+        SetTypicalTemperatureForAllDatasetsSection(), SaveDataFieldsByVariateTypeSection()
+    )
+    templates = {}
+    for tier in TIER_NAMES:
+        templates[tier] = EstimationTemplates(
+        mydata=MultitierMyDataSubstitutionTemplate(
+            source=BASE_TEMPLATE_FOLDER / tier / f'mydata_{species_name}.m',
+            sections=mydata_sections,
+        ),
+
+        pars_init=MultitierParsInitSubstitutionTemplate(
+            source=BASE_TEMPLATE_FOLDER / tier / f'pars_init_{species_name}.m',
+        ),
+
+        predict=CopyFileTemplate(BASE_TEMPLATE_FOLDER / tier / f'predict_{species_name}.m'),
+
+        run=RunSubstitutionTemplate(
+            source=BASE_TEMPLATE_FOLDER / tier / f'run_{species_name}.m'
+        ),
+    )
+
+    return templates
 
 
 def create_tier_structure(data, matlab_session='auto') -> MultiTierStructure:
@@ -50,10 +82,18 @@ def create_tier_structure(data, matlab_session='auto') -> MultiTierStructure:
         'individual': ['p_Am', 'kap_X']
     }
 
-    multitier = MultiTierStructure(species_name='Bos_taurus_Angus', entity_hierarchy=entity_hierarchy, data=data,
+    # estimation_templates = build_estimation_templates_from_folder(
+    #     template_folder=BASE_TEMPLATE_FOLDER,
+    #     tier_names=TIER_NAMES,
+    #     species_name='Bos_taurus_Angus',
+    # )
+
+    estimation_templates = build_estimation_templates(species_name=SPECIES_NAME)
+
+    multitier = MultiTierStructure(species_name=SPECIES_NAME, entity_hierarchy=entity_hierarchy, data=data,
                                    pars=initial_pars,
                                    tier_pars=tier_pars,
-                                   template_folder=BASE_TEMPLATE_FOLDER,
+                                   estimation_templates=estimation_templates,
                                    output_folder=ESTIMATION_FOLDER,
                                    matlab_session=matlab_session)
 

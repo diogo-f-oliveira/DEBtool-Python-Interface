d53a6f9f5edd09285c9efb64512a2b9a1ed91c83
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-04-10 17:44:14 +0100

    TypicalTemperatureSection can now format temperatures in Celsius

--- src/DEBtoolPyIF/estimation_files/mydata_temperature_sections.py
+++ src/DEBtoolPyIF/estimation_files/mydata_temperature_sections.py
@@ -8,10 +8,14 @@ from ..utils.mydata_code_generation import generate_meta_data_code
 class TypicalTemperatureSection(MyDataSection):
     key = "typical_temperature_block"
 
-    def __init__(self, *, t_typical: int | float = 0) -> None:
+    def __init__(self, *, t_typical: int | float = 0, is_celsius: bool = False) -> None:
         self.t_typical = t_typical
+        self.is_celsius = is_celsius
+        converted_temperature = convert_numeric_array_to_matlab(self.t_typical)
+        if self.is_celsius:
+            converted_temperature = f"C2K({converted_temperature})"
         line = (
-            f"{generate_meta_data_code('T_typical', convert_numeric_array_to_matlab(self.t_typical)).rstrip()} "
+            f"{generate_meta_data_code('T_typical', converted_temperature).rstrip()} "
             "% K, body temperature"
         )
         super().__init__(matlab_code=line)
